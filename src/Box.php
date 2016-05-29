5c5d14cbb1a57134fe8e34b3a079aa5214f4a911
brianxautumn <shred@brianautumn.com>
2016-05-28 20:42:58 -0700

    More project setup

---
+++ src/Box.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Isolator;
+
+/**
+ * Description of Box
+ *
+ * @author Brian Parra
+ */
+class Box {
+    //put your code here
+}

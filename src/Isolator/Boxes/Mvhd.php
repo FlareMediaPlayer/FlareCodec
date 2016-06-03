9efd078abc73a4dc71b1e1c6206eda142c67e08e
brianxautumn <shred@brianautumn.com>
2016-06-03 15:12:16 -0700

    Started Working on Mvhd

--- src/Isolator/Boxes/Mvhd.php
+++ src/Isolator/Boxes/Mvhd.php
@@ -7,8 +7,19 @@ namespace Isolator\Boxes;
  *
  * @author Brian Parra
  */
-class Mvhd extends \Isolator\Box {
+class Mvhd extends \Isolator\FullBox {
     
+    private $creationTime;
+    private $modificationTime;
+    private $timescale;
+    private $duration;
+    private $rate = 65536; //0x00010000
+    private $volume = 256; //0x0100 always a short
+    private $matrix = [
+        65536, 0, 0, 0, 65536, 0 ,0 ,0 ,1073741824
+    ]; // Unity Matrix
+    private $nextTrackID = 0;
+            
     
 
     function __construct($file) {
@@ -20,8 +31,36 @@ class Mvhd extends \Isolator\Box {
     }
     
     public function loadData() {
-        
+
+        if ($this->largeSize) {
+            
+            $this->headerSize = 16; //4 size + 4 type + 8 extended size; 
+            
+        } else {
+            
+            $this->headerSize = 8; //4 size + 4 type 
+            
+        }
+
+        fseek($this->file, $this->offset + $this->headerSize);
+        $this->version = \Isolator\ByteUtils::readUnsignedByte($this->file);
+        $this->flags[0] = \Isolator\ByteUtils::readUnsignedByte($this->file);
+        $this->flags[1] = \Isolator\ByteUtils::readUnsignedByte($this->file);
+        $this->flags[2] = \Isolator\ByteUtils::readUnsignedByte($this->file);
     }
+    
+    public function getBoxDetails() {
+        
+        $details = [];
+        $details["Size"] = $this->size;
+        $details["Offset"] = $this->offset;
+        $details["Version"] = $this->version;
+        $details["Flags"] = $this->flags;
+
+        return $details;
+    }
+    
+    
 
 
 }

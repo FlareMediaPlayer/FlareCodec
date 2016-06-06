243ebab27f67e62f1527a94b6f3cf2b13aa4e374
brianxautumn <shred@brianautumn.com>
2016-06-06 12:14:11 -0700

    updated Mdia with new get child boxes method

--- src/Isolator/Boxes/Mdia.php
+++ src/Isolator/Boxes/Mdia.php
@@ -22,33 +22,8 @@ class Mdia extends \Isolator\Box {
 
         $headerLength = 8;
         $internalOffset = $this->offset + $headerLength;
-
-
-        do {
-            //Set the offset 
-
-            fseek($this->file, $internalOffset);
-
-            $boxSize = \Isolator\ByteUtils::readUnsingedInteger($this->file);
-            $boxType = \Isolator\ByteUtils::readBoxType($this->file);
-
-
-
-            if (array_key_exists($boxType, \Isolator\Box::$boxTable)) {
-
-
-                $newBox = \Isolator\Box::$boxTable[$boxType]->newInstance($this->file);
-                $newBox->container = $this;
-                $newBox->setSize($boxSize);
-                $newBox->setOffset($internalOffset);
-                $newBox->loadData();
-                $this->boxMap[] = $newBox;
-            }
-
-
-
-            $internalOffset += $boxSize;
-        } while (($internalOffset - $this->offset ) < $this->size);
+        $this->loadChildBoxes($internalOffset);
+        
     }
 
 }

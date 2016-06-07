c1506a702e39e24b78e748fb6cb213401a2ab16e
brianxautumn <shred@brianautumn.com>
2016-06-06 23:52:39 -0700

    Stsz now reads sample table

--- src/Isolator/Boxes/Stsz.php
+++ src/Isolator/Boxes/Stsz.php
@@ -7,10 +7,12 @@ namespace Isolator\Boxes;
  *
  * @author Brian Parra
  */
-class Stsz extends \Isolator\Box {
+class Stsz extends \Isolator\FullBox {
     
+    private $sampleSize;
+    private $sampleCount;
+    private $sampleTable;
     
-
     function __construct($file) {
         
         $this->boxType = \Isolator\Box::STSZ;
@@ -19,7 +21,28 @@ class Stsz extends \Isolator\Box {
     }
     
     public function loadData() {
-        
+        $this->readHeader();
+        $this->sampleSize = \Isolator\ByteUtils::readUnsingedInteger($this->file);
+        $this->sampleCount = \Isolator\ByteUtils::readUnsingedInteger($this->file);
+        if($this->sampleSize ==0){
+            for($i = 0; $i< $this->sampleCount; $i++){
+                $this->sampleTable[] = \Isolator\ByteUtils::readUnsingedInteger($this->file);
+            }
+        }
+    }
+    
+    public function getBoxDetails() {
+
+        $details = [];
+
+        $details["Size"] = $this->size;
+        $details["Offset"] = $this->offset;
+        $details["Version"] = $this->version;
+        $details["Flags"] = $this->flags;
+        $details["Sample Size"] = $this->sampleSize;
+        $details["Sample Count"] = $this->sampleCount;
+  
+        return $details;
     }
 
 }

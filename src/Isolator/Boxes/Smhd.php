a3ab20da7cd797316a08e68ceb89724f16c51024
brianxautumn <shred@brianautumn.com>
2016-06-13 09:47:29 -0700

    smhd now writes to file

--- src/Isolator/Boxes/Smhd.php
+++ src/Isolator/Boxes/Smhd.php
@@ -9,7 +9,7 @@ namespace Isolator\Boxes;
  */
 class Smhd extends \Isolator\FullBox {
 
-    private $balance;
+    private $balance = 0.0;
 
     function __construct($file) {
 
@@ -37,4 +37,20 @@ class Smhd extends \Isolator\FullBox {
         return $details;
     }
 
+    public function writeToFile() {
+        
+        $this->offset = ftell($this->file); //Save the file pointer
+        $this->size = 16; //its fixed to 16 bits no matter what
+        \Isolator\ByteUtils::writeUnsignedInteger($this->file, $this->size); //Write the box size place holder for now
+        \Isolator\ByteUtils::writeChars($this->file, $this->boxType); //Write the box type
+        
+        \Isolator\ByteUtils::writeUnsignedByte($this->file, $this->version); //Write the box version
+        \Isolator\ByteUtils::writeUnsignedByte($this->file, $this->flags[0]); //Write the box version
+        \Isolator\ByteUtils::writeUnsignedByte($this->file, $this->flags[1]); //Write the box version
+        \Isolator\ByteUtils::writeUnsignedByte($this->file, $this->flags[2]); //Write the box version //12 bytes so far
+        
+        \Isolator\ByteUtils::writeFixed8_8($this->file, $this->balance);
+        \Isolator\ByteUtils::padBytes($this->file, 2);
+        
+    }
 }

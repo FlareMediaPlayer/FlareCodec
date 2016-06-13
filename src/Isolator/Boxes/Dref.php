59a693fc79c64ac9db65405c6980f6618be59238
brianxautumn <shred@brianautumn.com>
2016-06-13 10:16:58 -0700

    Dinf Dref and URL should now correctly output to file

--- src/Isolator/Boxes/Dref.php
+++ src/Isolator/Boxes/Dref.php
@@ -9,7 +9,7 @@ namespace Isolator\Boxes;
  */
 class Dref extends \Isolator\FullBox {
     
-    private $entryCount;
+    private $entryCount = 0;
     private $dataEntries = [];
 
     function __construct($file) {
@@ -42,5 +42,39 @@ class Dref extends \Isolator\FullBox {
 
 
         return $details;
+    }
+    
+    public function writeToFile() {
+        $this->prepareForWriting();
+        foreach($this->boxMap as $box){
+            $box->writeToFile();
+        }
+        $this->finalizeWriting();
+    }
+    
+    public function prepareForWriting(){
+        
+        $this->entryCount = count($this->boxMap);
+        $this->offset = ftell($this->file); //Save the file pointer
+        \Isolator\ByteUtils::writeUnsignedInteger($this->file, 0); //Write the box size, place holder for now
+        \Isolator\ByteUtils::writeChars($this->file, $this->boxType); //Write the box type
+        
+        \Isolator\ByteUtils::writeUnsignedByte($this->file, $this->version); //Write the box version
+        \Isolator\ByteUtils::writeUnsignedByte($this->file, $this->flags[0]); 
+        \Isolator\ByteUtils::writeUnsignedByte($this->file, $this->flags[1]); 
+        \Isolator\ByteUtils::writeUnsignedByte($this->file, $this->flags[2]); 
+        
+        \Isolator\ByteUtils::writeUnsignedInteger($this->file, $this->entryCount); //Write the box size, place holder for now
+        
+    }
+    
+    public function finalizeWriting(){
+        
+        $boxEnd = ftell($this->file); // Save the current position
+        $this->size = $boxEnd - $this->offset;
+        fseek($this->file, $this->offset); //Reset write pointer to beginning of file
+        \Isolator\ByteUtils::writeUnsignedInteger($this->file, $this->size); //Overwrite the box size
+        fseek($this->file, $boxEnd); //Finally put the file pointer back at the end of the file
+
     }
 }
--- src/Isolator/Boxes/Url.php
+++ src/Isolator/Boxes/Url.php
@@ -7,18 +7,62 @@ namespace Isolator\Boxes;
  * @author Brian Parra
  */
 class Url extends \Isolator\FullBox {
+
     
-    
+    private $location;
 
     function __construct($file) {
-        
+
         $this->boxType = \Isolator\Box::URL;
+        $this->flags[2] = 1; // For now assume location is internal
         parent::__construct($file);
-        
     }
-    
+
     public function loadData() {
+        $this->readHeader();
+        if ($this->flags[2] == 1) {
+            $this->location = \Isolator\ByteUtils::readString($this->file, $this->size - 12);
+   
+        }
         
     }
 
+    public function getBoxDetails() {
+
+        $details = [];
+
+        $details["Size"] = $this->size;
+        $details["Offset"] = $this->offset;
+        $details["Version"] = $this->version;
+        $details["Flags"] = $this->flags;
+        if ($this->flags[2] == 1) {
+            $details["Location"] = "No external location";
+        } else {
+            $details["Location"] = $this->location;
+        }
+
+
+        return $details;
+    }
+
+    public function writeToFile() {
+        $this->offset = ftell($this->file); //Save the file pointer
+        
+        $this->size = 12;
+        if($this->flags[2] == 0){
+            $this->size += (strlen($this->name) + 1 );
+        }
+        \Isolator\ByteUtils::writeUnsignedInteger($this->file, $this->size); //Write the box size place holder for now
+        \Isolator\ByteUtils::writeChars($this->file, $this->boxType); //Write the box type
+        
+        \Isolator\ByteUtils::writeUnsignedByte($this->file, $this->version); //Write the box version
+        \Isolator\ByteUtils::writeUnsignedByte($this->file, $this->flags[0]); //Write the box version
+        \Isolator\ByteUtils::writeUnsignedByte($this->file, $this->flags[1]); //Write the box version
+        \Isolator\ByteUtils::writeUnsignedByte($this->file, $this->flags[2]); //Write the box version //12 bytes so far
+        
+        if ($this->flags[2] == 0) {
+            \Isolator\ByteUtils::writeCString($this->file, $this->name);
+   
+        }
+    }
 }

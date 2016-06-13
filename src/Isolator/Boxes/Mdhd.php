ed7a40a3673b4553c990826298bc7ade6f6280af
brianxautumn <shred@brianautumn.com>
2016-06-13 09:16:16 -0700

    Mdhd now writes to file, needs language decoder

--- src/Isolator/Boxes/Mdhd.php
+++ src/Isolator/Boxes/Mdhd.php
@@ -9,10 +9,10 @@ namespace Isolator\Boxes;
  */
 class Mdhd extends \Isolator\FullBox {
 
-    private $creationTime;
-    private $modificationTime;
-    private $timescale;
-    private $duration;
+    private $creationTime = 0;
+    private $modificationTime = 0;
+    private $timescale = 0;
+    private $duration = 0;
     private $language = [];
 
     function __construct($file) {
@@ -58,4 +58,29 @@ class Mdhd extends \Isolator\FullBox {
         return $details;
     }
 
+    public function writeToFile(){
+        
+        $this->offset = ftell($this->file); //Save the file pointer
+        
+        $this->offset = ftell($this->file); //Save the file pointer
+        $this->size = 32; //expand if version 1
+        \Isolator\ByteUtils::writeUnsignedInteger($this->file, $this->size); //Write the box size place holder for now
+        \Isolator\ByteUtils::writeChars($this->file, $this->boxType); //Write the box type
+        
+        \Isolator\ByteUtils::writeUnsignedByte($this->file, $this->version); //Write the box version
+        \Isolator\ByteUtils::writeUnsignedByte($this->file, $this->flags[0]); //Write the box version
+        \Isolator\ByteUtils::writeUnsignedByte($this->file, $this->flags[1]); //Write the box version
+        \Isolator\ByteUtils::writeUnsignedByte($this->file, $this->flags[2]); //Write the box version //12 bytes so far
+        
+        \Isolator\ByteUtils::writeUnsignedInteger($this->file, $this->creationTime);
+        \Isolator\ByteUtils::writeUnsignedInteger($this->file, $this->modificationTime);
+        \Isolator\ByteUtils::writeUnsignedInteger($this->file, $this->timescale);
+        \Isolator\ByteUtils::writeUnsignedInteger($this->file, $this->duration);
+        
+        \Isolator\ByteUtils::writeUnsignedShort($this->file, 0); // placeholder for language
+        
+        \Isolator\ByteUtils::padBytes($this->file, 2); //Pad 2 bytes
+
+        
+    }
 }
